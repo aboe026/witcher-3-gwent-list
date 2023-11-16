145820a3213d17548b35297f4dea47e35e3d8f47
Adam Boe <adamboe@outlook.com>
2023-11-15 21:03:15 -0800

    Factions JSON

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,9 @@
 import { cleanEnv, str } from 'envalid'
 
 export default cleanEnv(process.env, {
-  OUTPUT_FILE: str({
-    desc: 'The path on the filesystem to a file where the JSON representation of the Gwent cards list should be outputted',
-    default: 'cards.json',
+  OUTPUT_DIRECTORY: str({
+    desc: 'The path on the filesystem to a directory where the JSON representation of the Gwent cards list should be outputted',
+    default: '.',
   }),
   SPREADSHEET_PATH: str({
     desc: 'The path on the filesystem to the Excel spreadsheet file containing Gwent card list',
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import fs from 'fs/promises'
+import path from 'path'
 import XLSX from 'xlsx'
 
 import env from './env'
@@ -7,14 +8,8 @@ import env from './env'
 ;(async () => {
   try {
     const spreadSheetPath = env.SPREADSHEET_PATH
-    const jsonPath = env.OUTPUT_FILE
-
-    // verify files
     if (!(await fileExists(spreadSheetPath))) {
       throw Error(`Gwent card spreadsheet "${spreadSheetPath}" does not exist or is not accessible.`)
-    }
-    if (await fileExists(jsonPath)) {
-      await fs.rm(jsonPath)
     }
 
     // verify spreadsheet
@@ -22,16 +17,30 @@ import env from './env'
     if (!workbook.SheetNames.includes(SHEET_NAMES.Cards)) {
       throw Error(`Spreadsheet "${spreadSheetPath}" does not contain sheet "${SHEET_NAMES.Cards}"`)
     }
+    if (!workbook.SheetNames.includes(SHEET_NAMES.Factions)) {
+      throw Error(`Spreadsheet "${spreadSheetPath}" does not contain sheet "${SHEET_NAMES.Factions}"`)
+    }
     if (!workbook.SheetNames.includes(SHEET_NAMES.Types)) {
       throw Error(`Spreadsheet "${spreadSheetPath}" does not contain sheet "${SHEET_NAMES.Types}"`)
     }
 
     // convert XSLX to JSON
     const cardSheet = workbook.Sheets[SHEET_NAMES.Cards]
-    const json = XLSX.utils.sheet_to_json(cardSheet)
+    const cardJson = XLSX.utils.sheet_to_json(cardSheet)
+    const factionSheet = workbook.Sheets[SHEET_NAMES.Factions]
+    const factionJson = XLSX.utils.sheet_to_json(factionSheet)
 
     // write JSON to file
-    await fs.writeFile(env.OUTPUT_FILE, JSON.stringify(json, null, 2))
+    const cardJsonFile = path.join(env.OUTPUT_DIRECTORY, 'cards.json')
+    const factionJsonFile = path.join(env.OUTPUT_DIRECTORY, 'factions.json')
+    if (await fileExists(cardJsonFile)) {
+      await fs.rm(cardJsonFile)
+    }
+    if (await fileExists(factionJsonFile)) {
+      await fs.rm(factionJsonFile)
+    }
+    await fs.writeFile(cardJsonFile, JSON.stringify(cardJson, null, 2))
+    await fs.writeFile(factionJsonFile, JSON.stringify(factionJson, null, 2))
   } catch (err: unknown) {
     console.error(err)
     process.exit(1)
@@ -49,5 +58,6 @@ async function fileExists(path: string): Promise<boolean> {
 
 enum SHEET_NAMES {
   Cards = 'Cards',
+  Factions = 'Factions',
   Types = 'Types',
 }
